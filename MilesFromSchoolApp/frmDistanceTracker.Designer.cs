b6cd292e241f28bdf0f9f43f72b8e6853a173a9c
Wolfknightx <Wolfknightx@Gmail.com>
2016-06-06 17:58:43 -0500

    Started java project 1 - create an application which displays back to the user if they are close or far from MCTC.

---
+++ MilesFromSchoolApp/frmDistanceTracker.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MilesFromSchoolApp
+{
+    partial class DistanceTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(530, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many miles do you live from MCTC?";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(60, 130);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(180, 80);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(280, 130);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(180, 80);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(600, 50);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(100, 38);
+            this.txtUserInput.TabIndex = 2;
+            // 
+            // DistanceTracker
+            // 
+            this.AcceptButton = this.btnEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(829, 277);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.label1);
+            this.Name = "DistanceTracker";
+            this.Text = "Track Your Distance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtUserInput;
+    }
+}
+
